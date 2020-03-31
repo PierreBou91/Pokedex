32e720aa8de7074997b1c71c21a1d5780a597b5f
Alibek Jakupov <31309253+ajakupov@users.noreply.github.com>
2020-03-31 16:18:35 +0200

    Add Pokemon isActive parameter

--- app/src/main/java/edu/harvard/cs50/pokedex/Pokemon.java
+++ app/src/main/java/edu/harvard/cs50/pokedex/Pokemon.java
@@ -3,6 +3,7 @@ package edu.harvard.cs50.pokedex;
 public class Pokemon {
     private String name;
     private String url;
+    private boolean isActive;
 
     Pokemon(String name, String url) {
         this.name = name;
@@ -15,5 +16,13 @@ public class Pokemon {
 
     public String getUrl() {
         return url;
+    }
+    
+    public boolean getStatus() {
+        return this.isActive;
+    }
+    
+    public void setStatus(boolean isActive) {
+        this.isActive = isActive;
     }
 }
